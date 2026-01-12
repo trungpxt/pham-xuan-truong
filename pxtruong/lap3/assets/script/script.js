4ae1ac332b6ca64954e9f6881c2d329008ae9830
phạm xuân trường <96964410+trungpxt@users.noreply.github.com>
2026-01-12 20:08:26 +0700

    update night

--- pxtruong/lap3/assets/script/script.js
+++ pxtruong/lap3/assets/script/script.js
@@ -1,3 +1,36 @@
 document.getElementById('butchange').addEventListener('click',()=>{
     document.body.classList.toggle('lightmode')
 })
+
+const circle = document.querySelector('.circle');
+
+document.addEventListener('mousemove', function(e) {
+    const x = e.pageX; 
+    const y = e.pageY;
+
+    circle.style.left = x + 'px';
+    circle.style.top = y + 'px';
+});
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight; // Chiều cao trình duyệt
+      var elementTop = reveals[i].getBoundingClientRect().top; // Vị trí phần tử so với đỉnh
+      var elementVisible = 150; // Khoảng cách đệm (xuất hiện sớm hay muộn)
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  // Lắng nghe sự kiện cuộn chuột
+  window.addEventListener("scroll", reveal);
+  
+  // Gọi hàm ngay khi load trang để kiểm tra các phần tử ở ngay đầu trang
+  reveal();
