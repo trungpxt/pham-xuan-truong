0274840a3465bfe400bc306e71ea6e21ed0d8ea9
phạm xuân trường <96964410+trungpxt@users.noreply.github.com>
2026-01-17 00:23:32 +0700

    clean code

--- pxtruong/bai1/app/script/script.js
+++ pxtruong/bai1/app/script/script.js
@@ -1,12 +1,22 @@
 window.addEventListener('scroll', function() {
     const sections = document.querySelectorAll('.section');
     sections.forEach(section => {
-        // Kiểm tra nếu section hiển thị ít nhất 80% trong viewport
         if (section.getBoundingClientRect().top < window.innerHeight * 0.8) {
             section.classList.add('visible');
         } else {
-            // (Tùy chọn) Nếu muốn hiệu ứng chạy lại, xóa class khi scroll ra ngoài
-            // section.classList.remove('visible');
+            
         }
     });
 });
+let word = () => {
+    const name = ['e', 'l', 'd', 'e', 'r', ' ', 'r', 'i', 'n', 'g'];
+    let a = '';
+    
+    name.forEach((char, i) => {
+        setTimeout(() => {
+            a += char;
+            document.getElementById("ttle").innerHTML = a;
+        }, i * 200);
+    });
+};
+word();
