b4aace3acb85dd53aceb43af84ae602a4cb2cf28
phạm xuân trường <96964410+trungpxt@users.noreply.github.com>
2026-01-21 09:27:28 +0700

    Delete pham-xuan-truong-main directory

--- pham-xuan-truong-main/pxtruong/lap2/assets/script.js
+++
@@ -1,9 +0,0 @@
-const circle = document.querySelector('.circle');
-
-document.addEventListener('mousemove', function(e) {
-    const x = e.pageX; 
-    const y = e.pageY;
-
-    circle.style.left = x + 'px';
-    circle.style.top = y + 'px';
-});
